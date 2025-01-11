9e00bfa757259d9ea00b31f210187e928378fb11
tinchin40 <tinchin40@gmail.com>
2025-01-12 05:51:46 +0800

    新增切換3分頁的class active效果

--- todolist.js
+++ todolist.js
@@ -1,7 +1,40 @@
 //新增從輸入框抓值並放入陣列
 let data = []; //新增空陣列裝代辦事項
+let filterStatus = "all"; // "all" 全部, "completed" 未完成, "pending"已完成 更新三個分頁切換狀態
 const card_input = document.querySelector(".card>input");
 const btn_add = document.querySelector(".btn_add");
+
+//切換3分頁<li>按鈕的class效果--------------------------------------------------------
+const tab = document.querySelector(".tab");
+tab.addEventListener("click", function (e) {
+  if (e.target.nodeName === "UL") {
+    //console.log("你點到ul了");
+    return;
+  }
+
+  const btnGroup = document.querySelectorAll(".tab li");
+  //抓取2個切換分頁的<li>按鈕   1.全部 2.待完成 3.已完成
+
+  btnGroup.forEach((btn) => {
+    btn.classList.remove("active");
+    //先讓3個按鈕統一移除active這個class
+  });
+  e.target.classList.add("active");
+  //給點擊的那一個分頁按鈕再添加 一個class="active"
+});
+// 切換三個按鈕狀態
+const filter_all = document.querySelector(".filter-all"); //完成
+const filter_completed = document.querySelector(".filter-completed"); //未完成
+const filter_pending = document.querySelector(".filter-pending"); //已完成
+filter_all.addEventListener("click", function () {
+  filterStatus = "all";
+});
+filter_completed.addEventListener("click", function () {
+  filterStatus = "completed";
+});
+filter_pending.addEventListener("click", function () {
+  filterStatus = "pending";
+});
 
 btn_add.addEventListener("click", function () {
   if (card_input.value.trim() === "") {
@@ -18,8 +51,19 @@ btn_add.addEventListener("click", function () {
 //渲染畫面
 const list = document.querySelector(".list"); //插入代辦事項<li>
 function render() {
+  // 先根據篩選狀態過濾待辦事項
+  let filteredData = []; //創一個空陣列存放篩選後的資料
   let str = "";
-  data.forEach((item) => {
+
+  if (filterStatus === "all") {
+    filteredData = data;
+  } else if (filterStatus === "completed") {
+    filteredData = data.filter((item) => item.completed === false);
+  } else if (filterStatus === "pending") {
+    filteredData = data.filter((item) => item.completed === true);
+  }
+
+  filteredData.forEach((item) => {
     str += `<li>
     <label class="checkbox" for="">
       <input type="checkbox" />
